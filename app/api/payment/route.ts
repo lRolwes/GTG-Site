b13edf5dd428f95455a0f9cf83cb2a1583bc3516
Louis Rolwes <lbRolwes@gmail.com>
2024-11-23 19:59:12 -0600

    updating payment form

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -5,7 +5,7 @@ import crypto from 'crypto'
 function encrypt(text: string): string {
   const algorithm = 'aes-256-cbc'
   const key = Buffer.from(process.env.ENCRYPTION_KEY || '', 'hex')
-  const iv = crypto.randomBytes(16)
+  const iv = crypto.randomBytes(32)
   const cipher = crypto.createCipheriv(algorithm, key, iv)
   let encrypted = cipher.update(text, 'utf8', 'hex')
   encrypted += cipher.final('hex')
